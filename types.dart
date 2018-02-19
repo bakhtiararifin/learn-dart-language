733be5bbefb1be62c48c0ad0016177a3d237214b
Bakhtiar Arifin <arifin@tamasia.co.id>
2018-02-19 21:57:28 +0700

    types

---
+++ types.dart
@@ -0,0 +1,17 @@
+main() {
+  const int x = 1;
+  const double y = 1.1;
+  print('int $x, double $y');
+
+  const String string = 'text';
+  print('string $string');
+
+  const bool boolean = true;
+  print('boolean $boolean');
+
+  const List list = const [1, 2, 3];
+  print('list $list');
+
+  const Map map = const {'key': 'value'};
+  print('map $map');
+}
